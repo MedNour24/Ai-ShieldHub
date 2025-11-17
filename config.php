665a36a43f38c088f1b7ba73c4cc9f3230159c6c
salmariahiii <salma.riahi@esprit.tn>
2025-11-17 22:10:30 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,32 @@
+<?php
+class config
+{
+    private static $pdo = null;
+
+    public static function getConnexion()
+    {
+        if (!isset(self::$pdo)) {
+            $servername = "localhost";
+            $username = "root";
+            $password = ""; 
+            $dbname = "modulequiz";
+
+            try {
+                self::$pdo = new PDO(
+                    "mysql:host=$servername;dbname=$dbname;charset=utf8",
+                    $username,
+                    $password,
+                    [
+                        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+                    ]
+                );
+            } catch (Exception $e) {
+                // DO NOT use die(). Throw the error so the API can handle it.
+                throw new Exception("Database Error: " . $e->getMessage());
+            }
+        }
+        return self::$pdo;
+    }
+}
+?>
